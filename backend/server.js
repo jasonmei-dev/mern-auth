64531356da21395338a1fa7f33f565880f8fb331
jmei403 <jason.mei403@gmail.com>
2025-01-13 12:43:39 -0500

    add controllers and routes directories and error handler middleware

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,19 @@
 import express from 'express';
-const port = 5001;
+import dotenv from 'dotenv';
+import userRoutes from './routes/userRoutes.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+
+dotenv.config();
+
+const port = process.env.PORT || 5001;
 
 const app = express();
 
+app.use('/api/users', userRoutes);
+
 app.get('/', (req, res) => res.send('Server is ready'));
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
