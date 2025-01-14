3b59339ab19613e17f21240d8732d79191a0bc8d
jmei403 <jason.mei403@gmail.com>
2025-01-14 18:37:21 -0500

    react router and bootstrap setup, homescreen, header, hero, and form container components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,14 @@
-import React from 'react';
+import Header from './components/Header';
+import { Container } from 'react-bootstrap';
+import { Outlet } from 'react-router-dom';
 
 const App = () => {
   return (
     <>
-      <h1>MERN Auth</h1>
+      <Header />
+      <Container className="my-2">
+        <Outlet />
+      </Container>
     </>
   );
 };
